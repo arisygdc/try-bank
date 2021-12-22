48481600b9c1efd4b4d733e25d35fe93bd5a1940
arisygdc <arisycso@gmail.com>
2021-12-22 20:24:09 +0700

    permission

---
+++ controller/CreatePermission.go
@@ -0,0 +1,37 @@
+package controller
+
+import (
+	"net/http"
+	"try-bank/database/postgres"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type PermissionReq struct {
+	Name string `from:"name" json:"name" binding:"required"`
+}
+
+func (c Controller) CreatePermission(ctx *gin.Context) {
+	var req PermissionReq
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	if err := c.DBSource.Queries.CreatePermissionLevel(ctx, postgres.CreatePermissionLevelParams{
+		ID:   uuid.New(),
+		Name: req.Name,
+	}); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"message": "created",
+	})
+}
