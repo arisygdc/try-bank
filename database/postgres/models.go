024ad2e9324b6630c16c3847a84edc9ee625b287
arisygdc <arisycso@gmail.com>
2021-12-23 16:27:43 +0700

    add company phone

--- database/postgres/models.go
+++ database/postgres/models.go
@@ -35,6 +35,7 @@ type Company struct {
 	ID        uuid.UUID `json:"id"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
+	Phone     string    `json:"phone"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
