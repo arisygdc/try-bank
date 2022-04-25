f67c822bcd043d24d458351fe8df62b88e2869a2
arisygdc <arisycso@gmail.com>
2022-04-26 02:12:15 +0700

    implementing new repository

--- database/repository.go
+++ database/repository.go
@@ -2,15 +2,19 @@ package database
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"try-bank/config"
 	"try-bank/database/postgres/pgrepo"
+	"try-bank/database/postgresql"
 	"try-bank/request"
+
+	_ "github.com/lib/pq"
 )
 
-var (
-	ErrNotFoundDriver = errors.New("database driver not found")
-)
+const PostgreDriver = "postgres"
+
+var ErrNotFoundDriver = errors.New("database driver not found")
 
 type IRepo interface {
 	CreateLevel(ctx context.Context, req request.PermissionReq) error
@@ -21,11 +25,54 @@ type IRepo interface {
 	Transfer(ctx context.Context, req request.Transfer) error
 }
 
-func NewRepository(env config.Environment) (dbsource IRepo, err error) {
+// deprecated repository
+func NewRepo(env config.Environment) (dbsource IRepo, err error) {
 	if env.DBDriver == "postgres" {
 		dbsource, err = pgrepo.NewPostgres(env.DBDriver, env.DBSource)
 	} else {
 		err = ErrNotFoundDriver
 	}
 	return
+}
+
+// implementing new repository
+type Repository struct {
+	sqlConn *sql.DB
+	query   *postgresql.Queries
+}
+
+func NewRepository(env config.Environment, ctx context.Context) (Repository, error) {
+	if env.DBDriver == PostgreDriver {
+		createSqlconn, err := sql.Open(env.DBDriver, env.DBSource)
+		if err != nil {
+			return Repository{}, err
+		}
+
+		createQuerier := postgresql.New(createSqlconn)
+		return Repository{
+			sqlConn: createSqlconn,
+			query:   createQuerier,
+		}, nil
+	}
+	return Repository{}, ErrNotFoundDriver
+}
+
+func (repos Repository) Query() *postgresql.Queries {
+	return repos.query
+}
+
+func (repos Repository) QueryTx(ctx context.Context, statement func(*postgresql.Queries) error) error {
+	tx, err := repos.sqlConn.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	QueryTx := repos.query.WithTx(tx)
+	errorTx := statement(QueryTx)
+	if errorTx != nil {
+		tx.Rollback()
+		return errorTx
+	}
+	tx.Commit()
+	return nil
 }
