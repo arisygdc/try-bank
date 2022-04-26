09cf0dd33dcdb8c421353c4ce94f9450ba905b8c
arisygdc <arisycso@gmail.com>
2022-04-27 01:48:07 +0700

    create user and duplicate test

--- app_service/account_test.go
+++ app_service/account_test.go
@@ -7,37 +7,34 @@ import (
 	"try-bank/app_service/account"
 	"try-bank/config"
 	"try-bank/database"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestUserAccount(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	env, err := config.NewEnv("../")
 	defer cancel()
-	if err != nil {
-		t.Error(err)
-		t.Fail()
-	}
+	assert.Nil(t, err)
 
 	repos, err := database.NewRepository(env, ctx)
-	if err != nil {
-		t.Error(err)
-		t.Fail()
-	}
+	assert.Nil(t, err)
 
 	svc := account.New(repos)
-	lvlUser, err := svc.GetLevel(ctx, account.LevelClientUser)
-	if err != nil {
-		t.Error(err)
-		t.Fail()
-	}
+	accountType, err := svc.GetAccountType(ctx, account.LevelSilver)
+	assert.Nil(t, err, "cannot get account type")
 
-	svc.CreateUserAccount(ctx, account.CreateUserParam{
-		Firstname: "arisy",
-		Lastname:  "musyafa'",
-		Email:     "arisy@gdc.com",
-		Phone:     "081217827013",
-		Pin:       "025361",
-		Birth:     time.Now(),
-		Level:     lvlUser.Name,
-	})
+	register := account.CreateUserParam{
+		Firstname:   "arisy",
+		Lastname:    "musyafa'",
+		Email:       "arisy@gdc.com",
+		Phone:       "081217827013",
+		Pin:         "025361",
+		Birth:       time.Now(),
+		AccountType: accountType.ID,
+	}
+	_, err = svc.CreateCustomerAccount(ctx, register)
+	assert.Nil(t, err, "error create account")
+	_, err = svc.CreateCustomerAccount(ctx, register)
+	assert.Error(t, err)
 }
