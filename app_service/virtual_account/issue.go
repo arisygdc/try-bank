2746d5c7ccecb4cd3bf335e3d0f4cae12c5fbcba
arisygdc <arisycso@gmail.com>
2022-04-27 22:00:00 +0700

    issue payment va and check

--- app_service/virtual_account/issue.go
+++ app_service/virtual_account/issue.go
@@ -2,6 +2,8 @@ package virtualaccount
 
 import (
 	"context"
+	"errors"
+	"time"
 	"try-bank/database/postgresql"
 
 	"github.com/google/uuid"
@@ -13,6 +15,14 @@ type IssueVAPayment struct {
 	Payment_charge         float64
 }
 
+type IssuedPayment struct {
+	ID                   uuid.UUID
+	VirtualAccountID     uuid.UUID
+	VirtualAccountNumber int32
+	PaymentCharge        float64
+	IssuedAt             time.Time
+}
+
 func (svc Service) IssueVAPayment(ctx context.Context, param IssueVAPayment) error {
 	return svc.repos.Query().IssuePaymentVA(ctx, postgresql.IssuePaymentVAParams{
 		ID:                   uuid.New(),
@@ -20,4 +30,22 @@ func (svc Service) IssueVAPayment(ctx context.Context, param IssueVAPayment) err
 		VirtualAccountNumber: param.Virtual_account_number,
 		PaymentCharge:        param.Payment_charge,
 	})
+}
+
+func (svc Service) CheckIssuedVAPayment(ctx context.Context, virtualAccount_id uuid.UUID, virtualAccount_number int32, amount float64) (IssuedPayment, error) {
+	issuedPaymentParam := postgresql.CheckIssuedPaymentVAParams{
+		VirtualAccountID:     virtualAccount_id,
+		VirtualAccountNumber: virtualAccount_number,
+	}
+
+	issuedPayment, err := svc.repos.Query().CheckIssuedPaymentVA(ctx, issuedPaymentParam)
+	if err != nil {
+		return IssuedPayment{}, err
+	}
+
+	if issuedPayment.PaymentCharge != amount {
+		return IssuedPayment{}, errors.New("the amount paid does not match")
+	}
+
+	return IssuedPayment(issuedPayment), nil
 }
