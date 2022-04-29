9527364727b485f1092b18851b6d48a0ade7ebb5
arisygdc <arisycso@gmail.com>
2022-04-30 00:52:51 +0700

    remove un used

--- database/repository.go
+++ database/repository.go
@@ -5,9 +5,7 @@ import (
 	"database/sql"
 	"errors"
 	"try-bank/config"
-	"try-bank/database/postgres/pgrepo"
 	"try-bank/database/postgresql"
-	"try-bank/request"
 
 	_ "github.com/lib/pq"
 )
@@ -15,25 +13,6 @@ import (
 const PostgreDriver = "postgres"
 
 var ErrNotFoundDriver = errors.New("database driver not found")
-
-type IRepo interface {
-	CreateLevel(ctx context.Context, req request.PermissionReq) error
-	CreateUser(ctx context.Context, req request.PostUser, permission string) (int32, error)
-	CreateCompany(ctx context.Context, req request.PostCompany) (int32, error)
-	ActivateVA(ctx context.Context, req request.VirtualAccount) (int32, string, error)
-	PaymentVA(ctx context.Context, req request.PaymentVA) error
-	Transfer(ctx context.Context, req request.Transfer) error
-}
-
-// deprecated repository
-func NewRepo(env config.Environment) (dbsource IRepo, err error) {
-	if env.DBDriver == "postgres" {
-		dbsource, err = pgrepo.NewPostgres(env.DBDriver, env.DBSource)
-	} else {
-		err = ErrNotFoundDriver
-	}
-	return
-}
 
 // implementing new repository
 type IRepository interface {
--- main.go
+++ main.go
@@ -19,20 +19,14 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	// deprecated
-	repo, err := database.NewRepo(env)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	deprecatedCtr := controller.New(repo)
-
 	repository, err := database.NewRepository(env)
 	if err != nil {
 		log.Fatal(err)
 	}
-	svc := appservice.NewService(repository)
-	ctr := controller.NewController(svc)
-	server := server.NewServer(env, ctr, deprecatedCtr)
+
+	service := appservice.NewService(repository)
+	controller := controller.NewController(service)
+	server := server.NewServer(env, controller)
 	server.ApiV1Route()
 	server.Run()
 }
