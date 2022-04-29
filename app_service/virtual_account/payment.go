2110e2a11b805c128d695dc8ce073589fae4de65
arisygdc <arisycso@gmail.com>
2022-04-30 00:05:49 +0700

    add todo

--- app_service/virtual_account/payment.go
+++ app_service/virtual_account/payment.go
@@ -14,7 +14,9 @@ type PaidVA struct {
 	PaidAt          time.Time
 }
 
-// TODO use transaction to subtract payer wallet and increase company wallet
+// TODO
+// use transaction to subtract payer wallet and increase company wallet
+// notify company using callback_url
 func (svc Service) PaymentVirtualAccount(ctx context.Context, IssuedPayment_id uuid.UUID) (PaidVA, error) {
 	paid, _ := svc.repos.Query().PaymentVA(ctx, IssuedPayment_id)
 	return PaidVA(paid), errors.New("<- todo")
