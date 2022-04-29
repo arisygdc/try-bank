b8203fa4dc47336530137431c42c41e776d6c9c7
arisygdc <arisycso@gmail.com>
2022-04-29 21:46:11 +0700

    company register virtual account

--- controller/virtual_account/register.go
+++ controller/virtual_account/register.go
@@ -2,17 +2,38 @@ package virtualaccount
 
 import (
 	"net/http"
-	"try-bank/request"
 
 	"github.com/gin-gonic/gin"
 )
 
-func (va VirtualAccountController) Register(ctx *gin.Context) {
-	var req request.RegisterVirtualAccount
+type RegisterVirtualAccount struct {
+	RegisteredNumber int32  `json:"registered_number" binding:"required"`
+	CallbackURL      string `json:"callback_url" binding:"required"`
+}
+
+func (ctr VirtualAccountController) Register(ctx *gin.Context) {
+	var req RegisterVirtualAccount
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
-			"message": err,
+			"error": err,
 		})
+		return
 	}
 
+	ca, err := ctr.service.Company.CompanyAccount(ctx, req.RegisteredNumber)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+	registered, err := ctr.service.VirtualAccount.Register(ctx, ca.CompanyID, req.CallbackURL)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"va_identity":         registered.Identity,
+		"va_authorizationKey": registered.AuthorizationKey,
+		"callback_url":        registered.Callback_url,
+	})
 }
