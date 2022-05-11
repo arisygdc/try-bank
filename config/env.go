f2945cd2f6459d9404cb7269b72a4754c3f25ee6
arisygdc <arisycso@gmail.com>
2022-05-11 17:41:44 +0700

    config

--- config/env.go
+++ config/env.go
@@ -9,8 +9,17 @@ type Environment struct {
 	Environment   string `mapstructure:"ENVIRONMENT"`
 }
 
-func NewEnv(path string) (env Environment, err error) {
+// Supply param for configuration from .env file or environment variable if exists
+// default config file name config.env
+
+// configName param when to use custom name
+// example: file name is app.env
+// fill "app" in configName param
+func NewEnv(path string, configName ...string) (env Environment, err error) {
 	viper.AddConfigPath(path)
+	if len(configName) == 1 {
+		viper.SetConfigName(configName[0])
+	}
 	viper.SetConfigType("env")
 	viper.AutomaticEnv()
 	err = viper.ReadInConfig()
